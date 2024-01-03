dc3b93eb475610e92b680b3fc3c08969424ea240
Manish Ahire <ahiremanishb@gmail.com>
2024-01-03 12:43:56 +0530

    Assignment-1, 7. DivisionOfTwoNumbers

---
+++ Assignment - 1/7. DivisionOfTwoNumbers.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+/*
+
+    Write a program that accepts two numbers and prints its division.
+     
+    Input:
+         Enter the first number: 22
+         Enter the second number: 11
+     
+    Output:
+         The division of 22 and 11 is: 2
+
+*/
+
+func divisionOfTwoNumbers(number1: Int, number2: Int) {
+    
+    let division = number1 / number2
+    
+    print("The division of \(number1) and \(number2) is: \(division)")
+}
+
+divisionOfTwoNumbers(number1: 22, number2: 11)
+
+
