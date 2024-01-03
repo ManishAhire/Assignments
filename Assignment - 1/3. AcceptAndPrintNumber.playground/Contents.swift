465d1d0493ef9459d4f9543d16102182985b8b21
Manish Ahire <ahiremanishb@gmail.com>
2024-01-03 11:36:40 +0530

    Assignment-1, 3. AcceptAndPrintNumber

---
+++ Assignment - 1/3. AcceptAndPrintNumber.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+/*
+    Write a program that accepts a number from the user and prints that number.
+    
+    Input:
+         Enter the number: 2211
+    
+    Output:
+         The entered number is: 2211
+
+*/
+
+
+func printNumber(number: Int) {
+    print("The entered number is: \(number)")
+}
+
+printNumber(number: 2211)
