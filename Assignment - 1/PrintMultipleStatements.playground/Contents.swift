07052259a002e30709bf566d3428bdafa4befec8
Manish Ahire <ahiremanishb@gmail.com>
2024-01-03 11:21:24 +0530

    Initial Commit

---
+++ Assignment - 1/PrintMultipleStatements.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+/*
+    Write a program that prints multiple sentences on the console.
+     
+    Output:
+         Hello, Manish Ahire.
+         Learn Swift Programming:)
+
+ */
+
+print("Hello, Manish Ahire.")
+print("Learn Swift Programming:)")
---
+++ Assignment - 1/PrintSingleStatement.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+/*
+ 
+ Write a program that print sentence on the console.
+ 
+     Output:
+         Hello, Manish Ahire.
+
+*/
+
+
+print("Hello, Manish Ahire")
