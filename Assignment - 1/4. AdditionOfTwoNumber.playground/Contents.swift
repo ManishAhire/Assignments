78eafae51e85f5c90441aff2676876f54809297c
Manish Ahire <ahiremanishb@gmail.com>
2024-01-03 11:50:18 +0530

    Assignment-1 5. SubtractionOfTwoNumbers

--- Assignment - 1/4. AdditionOfTwoNumber.playground/Contents.swift
+++ Assignment - 1/4. AdditionOfTwoNumber.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+ 
+    Write a program that accepts two numbers and prints its addition.
+ 
+    Input:
+         Enter the first number: 22
+         Enter the second number: 11
+    
+    Output:
+         The addition of 22 and 11 is: 33
+ 
+*/
+
+
+func additionOfTwoNumbers(number1 : Int, number2 : Int) {
+    
+    let addition = number1 + number2
+    
+    print("The addition of \(number1) and \(number2) is: \(addition)")
+    
+}
+
+additionOfTwoNumbers(number1: 22, number2: 11)
--- Assignment - 1/5. SubtractionOfTwoNumbers.playground/Contents.swift
+++ Assignment - 1/5. SubtractionOfTwoNumbers.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+    
+    Write a program that accepts two numbers and prints its subtraction.
+    
+    Input:
+         Enter the first number: 22
+         Enter the second number: 11
+    
+    Output:
+         The subtraction of 22 and 11 is: 11
+
+*/
+
+func subtractionOfTwoNumbers(number1: Int, number2: Int) {
+    
+    let subtraction = number1 - number2
+    
+    print("The subtraction of \(number1) and \(number2) is: \(subtraction)")
+}
+
+subtractionOfTwoNumbers(number1: 22, number2: 11)
